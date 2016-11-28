91d56152bef03156e8d524e0056c9c4bf5f77e58
mike <viriyananda.michael@gmail.com>
2016-11-28 15:51:56 +0700

    Adding module account_payment_order_open_chatter
    Fix minor comments

---
+++ account_payment_order_open_chatter/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Add Open Chatter to Payment Order",
+    "version": "8.0.1.0.0",
+    "summary": "Add Open Chatter to Payment Order",
+    "category": "Accounting & Finance",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["account_payment"],
+    "data": [
+        "views/payment_order_view.xml"
+    ],
+}
