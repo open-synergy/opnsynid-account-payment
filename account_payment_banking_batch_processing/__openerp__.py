ebdb993b85e35024f32c9bbc8d73a78c92eb696d
Andhitia Rama <andhitia.r@gmail.com>
2019-05-23 22:08:51 +0700

    [8.0.1.0.0] account_payment_banking_batch_processing

---
+++ account_payment_banking_batch_processing/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Payment Order To Banking Batch Processing",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_payment",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/select_batch_processing.xml",
+        "wizards/batch_processing.xml",
+        "views/payment_banking_batch_processing_views.xml",
+        "views/payment_order_views.xml",
+    ],
+}
