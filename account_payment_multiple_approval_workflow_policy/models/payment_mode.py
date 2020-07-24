a1584378b4a537b09f24a1b8846c3284d149070d
mikevhe18 <viriyananda.michael@gmail.com>
2020-07-24 10:23:42 +0700

    [8.0.1.0.0][account_payment_multiple_approval_workflow_policy]

---
+++ account_payment_multiple_approval_workflow_policy/models/payment_mode.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class PaymentMode(models.Model):
+    _inherit = "payment.mode"
+
+    payment_order_confirm_approval_grp_ids = fields.Many2many(
+        string="Allowed to Confirm",
+        comodel_name="res.groups",
+        relation="rel_payment_mode_2_grp_payment_order_confirm_approval",
+        column1="mode_id",
+        column2="group_id",
+    )
+    payment_order_restart_approval_grp_ids = fields.Many2many(
+        string="Allow To Restart Approval",
+        comodel_name="res.groups",
+        relation="rel_payment_mode_2_grp_payment_order_restart_approval",
+        column1="mode_id",
+        column2="group_id",
+    )
