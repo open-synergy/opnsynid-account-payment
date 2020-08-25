db945929d239cafb5e0386a75bb0395aa6a445fc
mikevhe18 <viriyananda.michael@gmail.com>
2020-08-25 23:00:11 +0700

    [8.0.2.0.0][account_payment_banking_batch_processing] Fixing Travis Error

--- account_payment_banking_batch_processing/migrations/8.0.2.0.0/post-migration.py
+++ account_payment_banking_batch_processing/migrations/8.0.2.0.0/post-migration.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from openupgradelib import openupgrade
 from openerp import api, SUPERUSER_ID
+
 
 @openupgrade.migrate()
 def migrate(cr, version):
