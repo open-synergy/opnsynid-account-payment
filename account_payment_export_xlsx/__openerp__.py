a842af30bc7d30e9cb1e21032926b8462d43cb44
Andhitia Rama <andhitia.r@gmail.com>
2020-08-26 16:56:04 +0700

    [account_payment_export_xlsx] 8.0.1.0.0

---
+++ account_payment_export_xlsx/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Payment Order Export to xlsx",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_payment",
+        "base_export_xlsx",
+    ],
+    "data": [
+        "views/payment_order_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
